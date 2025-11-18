fe5ea45ec7fe8aa56a83ff7905e9b4ae25056650
saralozi <saralozi08@gmail.com>
2025-11-18 01:24:17 +0100

    Adding solution for task 13

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,3 +1,6 @@
 -- Write a script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
 
--- The database name will be passed as an argument of the mysql command
+-- Delete all records of table filtering by score
+DELETE
+FROM second_table
+WHERE score <= 5
