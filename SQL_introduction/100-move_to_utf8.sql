5b0555c1e64e009af0eec819c2c8aa08a1df097a
saralozi <saralozi08@gmail.com>
2025-11-18 18:53:14 +0100

    Adding solution for advanced tasks

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,19 @@
+-- A script that converts hbtn_0c_0 database to UTF8.
+
+-- Convert database to UTF8
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
+-- Convert first_table to UTF8
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+-- Convert name field to UTF8
+ALTER TABLE first_table
+    MODIFY name VARCHAR(256)
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
