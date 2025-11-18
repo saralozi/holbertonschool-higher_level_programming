1485a9aef2113d0160f1f7eb946a0f4da327e4b3
saralozi <saralozi08@gmail.com>
2025-11-18 01:22:24 +0100

    Adding solution for task 12

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,6 @@
 -- Write a script that updates the score of Bob to 10 in the table second_table.
 
--- You are not allowed to use Bob’s id value, only the name field
--- The database name will be passed as an argument of the mysql command
+-- Update a record in table by changing score value filtering by name
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
