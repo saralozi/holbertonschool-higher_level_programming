4c081b87f5448841d0384e50e4032a0a82a598ff
saralozi <saralozi08@gmail.com>
2025-11-18 01:19:05 +0100

    Adding solution for task 11

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Write a script that updates the score of Bob to 10 in the table second_table.
+
+-- You are not allowed to use Bob’s id value, only the name field
+-- The database name will be passed as an argument of the mysql command
